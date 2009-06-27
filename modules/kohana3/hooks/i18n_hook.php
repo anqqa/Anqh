aa536b1e929219347a21ba065fbc08c3ae1555a6
anqqa <anttiq@gmail.com>
2009-06-27 15:28:22 +0300

    I18n Kohana 3 functionality to Kohana 2

---
+++ modules/kohana3/hooks/i18n_hook.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Kohana translation/internationalization function.
+ *
+ * __('Welcome back, :user', array(':user' => $username));
+ *
+ * @param   string  text to translate
+ * @param   array   values to replace in the translated text
+ * @return  string
+ */
+function __($string, array $values = NULL)
+{
+  if (I18n::$lang !== I18n::$default_lang)
+  {
+    // Get the translation for this string
+    $string = I18n::get($string);
+  }
+
+  return empty($values) ? $string : strtr($string, $values);
+}
