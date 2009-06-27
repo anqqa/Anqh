e97be9dfdef92226f00cdf54e653f13671d3328c
anqqa <anttiq@gmail.com>
2009-06-28 01:28:59 +0300

    Move page actions for float

--- modules/anqh/views/layout.php
+++ modules/anqh/views/layout.php
@@ -3,14 +3,14 @@
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?= $page_title ?><?= (!empty($page_title) ? ' | ' : '') . Kohana::config('site.site_name') ?></title>
+	<title><?= strip_tags($page_title) ?><?= (!empty($page_title) ? ' | ' : '') . Kohana::config('site.site_name') ?></title>
 	<link rel="icon" type="image/png" href="/ui/favicon.png" />
-	<?= html::stylesheet(array('ui/boot', 'ui/grid', 'ui/typo', 'ui/base', 'ui/site', 'ui/jquery-ui')); ?> 
-	<?= html::stylesheet($stylesheets) ?> 
+	<?= html::stylesheet(array('ui/boot', 'ui/grid', 'ui/typo', 'ui/base', 'ui/site', 'ui/jquery-ui')); ?>
+	<?= html::stylesheet($stylesheets) ?>
 	<script type="text/javascript" src="http://www.google.com/jsapi?key=<?= Kohana::config('site.google_api_key') ?>"></script>
-	<?= html::script('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js'); ?> 
-	<?= html::script('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js') ?> 
-<?= widget::get('head') ?> 
+	<?= html::script('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js'); ?>
+	<?= html::script('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js') ?>
+<?= widget::get('head') ?>
 </head>
 
 <body id="<?= $page_id ?>" class="<?= $page_class ?>">
@@ -21,35 +21,36 @@
 		<div class="container-12 clearfix">
 
 <h1><?= html::anchor('/', Kohana::config('site.site_name')) ?></h1>
-<?= widget::get('header') ?> 
-<?= widget::get('navigation') ?> 
+<?= widget::get('header') ?>
+<?= widget::get('navigation') ?>
 
 		</div>
 	</div>
-	
+
 	<!-- /HEADER -->
 
 
 	<!-- CONTENT -->
 
 	<div id="content" class="container-1">
-	
+
 		<div class="container-12 clearfix breadcrumb">
 			<div class="grid-12">
-			
-<?= widget::get('breadcrumb') ?> 
+
+<?= widget::get('breadcrumb') ?>
 
 			</div>
 		</div>
-		
+
 		<div class="container-12 clearfix">
 			<div id="main-content" class="grid-10-full">
-			
+
 				<div id="title" class="grid-10">
-				
+
+<?= widget::get('actions') ?>
+
 					<h2><?= $page_title ?></h2>
 					<p class="subtitle"><?= $page_subtitle ?></p>
-<?= widget::get('actions') ?>
 <?= widget::get('tabs') ?>
 				</div>
 
@@ -58,22 +59,22 @@
 <?= widget::get('subnavigation') ?>
 
 				</div>
-				
+
 <?= $content ?>
-			
+
 			</div>
 
-			
+
 			<!-- SIDE ADS -->
-			
+
 			<div id="right-ads" class="grid-2 side">
-			
-<?= widget::get('side_ads') ?> 
+
+<?= widget::get('side_ads') ?>
 
 			</div>
-			
+
 			<!-- /SIDE ADS -->
-		
+
 		</div>
 	</div>
 
@@ -86,12 +87,12 @@
 		<div class="container-12 clearfix">
 			<div class="grid-6 actions">
 
-<?= widget::get('dock') ?> 
+<?= widget::get('dock') ?>
 
 			</div>
 			<div class="grid-6 extra-actions">
 
-<?= widget::get('dock2') ?> 
+<?= widget::get('dock2') ?>
 
 			</div>
 		</div>
@@ -106,11 +107,11 @@
 		<div class="container-12 clearfix">
 
 <?= widget::get('navigation') ?>
-<?= widget::get('footer') ?> 
+<?= widget::get('footer') ?>
 
 		</div>
 	</div>
-	
+
 	<!-- /FOOTER -->
 
 
@@ -119,28 +120,28 @@
 	<div id="end" class="container-1">
 		<div class="container-12 clearfix">
 
-<?= widget::get('end') ?> 
+<?= widget::get('end') ?>
 
 		</div>
 	</div>
-	
+
 	<!-- /END -->
 
-	
-<?= html::script(array('js/jquery.autocomplete.pack', 'js/jquery.hint')) ?> 
-<?= html::script_source("$('input:text,input:password,textarea').hint('hint');"); ?> 
-	
+
+<?= html::script(array('js/jquery.autocomplete.pack', 'js/jquery.hint')) ?>
+<?= html::script_source("$('input:text,input:password,textarea').hint('hint');"); ?>
+
 <script type="text/javascript">
 /*
 $(function() {
 	var header = $('#header');
 	header.css({ position: 'absolute' });
-	
+
 	var PADDING = 102;
 	var FIXED = (header.css('position') == 'fixed');
-	
+
 	var win = $(window);
-	
+
 	win.scroll(function() {
 		if (win.scrollTop() > PADDING) {
 			if ($.browser.msie && $.browser.version == '6.0') {
@@ -161,8 +162,8 @@ $(function() {
 });
 */
 </script>
-	
-<?= widget::get('foot') ?> 
+
+<?= widget::get('foot') ?>
 
 </body>
 </html>
