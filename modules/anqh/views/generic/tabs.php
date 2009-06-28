277c259328b18c9ea6769f345ff477c9e8fe715c
anqqa <anttiq@gmail.com>
2009-06-28 20:22:31 +0300

    Support for selected tab on load and hideable tabs

--- modules/anqh/views/generic/tabs.php
+++ modules/anqh/views/generic/tabs.php
@@ -1,11 +1,12 @@
 <div class="tabs" id="<?= $id ?>">
 	<ul>
-<?php	foreach ($tabs as $tab): ?>
-		<li><a href="<?= $tab['href'] ?>"><?= $tab['title'] ?></a></li>
-<?php	endforeach; ?>
+		<?php	$t = 0; $selected = 0; foreach ($tabs as $tab): $selected = !empty($tab['selected']) ? $t : $selected; $t++; ?>
+		<li><?= html::anchor($tab['href'], $tab['title']) ?></li>
+		<?php	endforeach; ?>
 	</ul>
-<?php	foreach ($tabs as $tab): ?>
+	<?php	foreach ($tabs as $tab): ?>
 	<?= $tab['tab'] ?>
-<?php	endforeach; ?>
+	<?php	endforeach; ?>
 </div>
-<?php widget::add('foot', html::script_source('$(function() { $("#' . $id . '").tabs({ fx: { height: "toggle", opacity: "toggle", duration: "fast" } }); });'));
+<?php
+widget::add('foot', html::script_source('$(function() { $("#' . $id . '").tabs({ selected: ' . $selected . ', collapsible: true, fx: { height: "toggle", opacity: "toggle", duration: "fast" } }); });'));
