6222cda92f3ed1c9fbe2fc3ecbc81870c00071c3
anqqa <anttiq@gmail.com>
2009-10-18 22:11:03 +0300

    Avoid ugly flickering when initializing tabs

--- modules/anqh/views/generic/tabs.php
+++ modules/anqh/views/generic/tabs.php
@@ -1,12 +1,14 @@
-<div class="tabs" id="<?= $id ?>">
+
+<div class="mod tabs" id="<?= $id ?>">
 	<ul>
 		<?php	$t = 0; $selected = 0; foreach ($tabs as $tab): $selected = !empty($tab['selected']) ? $t : $selected; $t++; ?>
 		<li><?= html::anchor($tab['href'], $tab['title']) ?></li>
 		<?php	endforeach; ?>
 	</ul>
-	<?php	foreach ($tabs as $tab): ?>
-	<?= $tab['tab'] ?>
-	<?php	endforeach; ?>
+	<?php	foreach ($tabs as $tab) echo $tab['tab']; ?>
 </div>
 <?php
-widget::add('foot', html::script_source('$(function() { $("#' . $id . '").tabs({ selected: ' . $selected . ', collapsible: true, fx: { height: "toggle", opacity: "toggle", duration: "fast" } }); });'));
+// Initialize tabs immediately to aviod ugly jumping
+echo html::script_source('$("#' . $id . '").tabs({ selected: ' . $selected . ', collapsible: true, fx: { height: "toggle", opacity: "toggle", duration: "fast" } });');
+// widget::add('foot', html::script_source('$(function() { $("#' . $id . '").tabs({ selected: ' . $selected . ', collapsible: true, fx: { height: "toggle", opacity: "toggle", duration: "fast" } }); });'));
+// widget::add('foot', html::script_source('$(function() { $("#' . $id . ' ul").tabs("#' . $id . ' > div", { initialIndex: ' . $selected . ', effect: "fade" }); });'));
