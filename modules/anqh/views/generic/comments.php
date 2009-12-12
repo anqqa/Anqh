3a8fd62851c7e1b059b2a3734f4e5553327ccf86
anqqa <anttiq@gmail.com>
2009-12-12 13:31:55 +0200

    Moved from member comments to generic comments view

---
+++ modules/anqh/views/generic/comments.php
@@ -0,0 +1,80 @@
+
+<section class="mod comments">
+
+	<header>
+		<h3><?= __('Comments') ?></h3>
+
+		<?= $pagination ?>
+
+	</header>
+
+	<?= form::open() ?>
+	<fieldset class="horizontal">
+		<ul>
+			<?php if ($private): ?>
+			<?= form::checkbox_wrap('private', '1', $values, "onchange=\"$('#comment').toggleClass('private', this.checked)\"", '<abbr class="private" title="' . __('Private comment') . '">' . __('Priv') . '</abbr>') ?>
+			<?php endif; ?>
+
+			<?= form::input_wrap('comment', '', 'maxlength="300"', '', $errors) ?>
+
+			<li><?= form::submit(false, __('Comment')) ?></li>
+		</ul>
+		<?= form::csrf() ?>
+	</fieldset>
+	<?= form::close() ?>
+
+	<ul>
+	<?php foreach ($comments as $comment):
+		$classes = array('line');
+
+		if (!$comment->private || $this->user && in_array($this->user->id, array($comment->user_id, $comment->author_id))):
+
+			if ($comment->private) {
+				$classes[] = 'private';
+			}
+
+			// Viewer's post
+			if ($this->user && $comment->author_id == $this->user->id) {
+				$classes[] = 'my';
+				$mine = true;
+			} else {
+				$mine = false;
+			}
+
+			// Topic author's post
+			if ($comment->author_id == $comment->user_id) {
+				$classes[] = 'owner';
+			}
+ 	?>
+
+		<li class="<?= implode(' ', $classes) ?>">
+
+			<?php if ($this->user && $comment->user_id == $this->user->id || $mine): ?>
+			<span class="actions">
+				<?= html::anchor(sprintf($delete, $comment->id), __('Delete'), array('class' => 'action comment-delete')) ?>
+			</span>
+			<?php endif; ?>
+
+			<?= html::avatar($comment->author->avatar, $comment->author->username) ?>
+
+			<?= html::nick($comment->author_id, $comment->author->username) ?>,
+			<?= __(':ago ago', array(
+				':ago' => html::time(date::timespan_short($comment->created), $comment->created))
+			) ?>
+			<br />
+
+			<?= $comment->private ? '<abbr title="' . __('Private comment') . '">' . __('Priv') . '</abbr>: ' : '' ?>
+			<?= text::smileys(text::auto_link(html::specialchars($comment->comment))) ?>
+
+		</li>
+
+	<?php endif; endforeach; ?>
+	</ul>
+
+	<footer>
+
+		<?= $pagination ?>
+
+	</footer>
+
+</section
