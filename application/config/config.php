8759932bd791baaf4697acd289a0f019d4c89dbb
anqqa <anttiq@gmail.com>
2009-12-20 22:32:10 +0200

    More 2.3.4 modules

--- application/config/config.php
+++ application/config/config.php
@@ -73,21 +73,6 @@ $config['global_xss_filtering'] = TRUE;
 $config['enable_hooks'] = TRUE;
 
 /**
- * Log thresholds:
- *  0 - Disable logging
- *  1 - Errors and exceptions
- *  2 - Warnings
- *  3 - Notices
- *  4 - Debugging
- */
-$config['log_threshold'] = IN_PRODUCTION ? 2 : 3;
-
-/**
- * Message logging directory.
- */
-$config['log_directory'] = APPPATH . '../logs';
-
-/**
  * Enable or disable displaying of Kohana error pages. This will not affect
  * logging. Turning this off will disable ALL error pages.
  */
@@ -120,6 +105,8 @@ $config['config_drivers'] = array();
  */
 $config['modules'] = array(
 	MODPATH . 'postgres', // PostgreSQL support
+	MODPATH . 'calendar', // Calendar from 2.3.4
+	MODPATH . 'pagination', // Pagination from 2.3.4
 	MODPATH . 'gmaps',    // Google Maps integration
 
 	MODPATH . 'anqh',     // Anqh core
