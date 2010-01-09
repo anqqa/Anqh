94f0604abf54c1875cfea01b2138fe24bb7fac33
anqqa <anttiq@gmail.com>
2010-01-09 21:06:31 +0200

    Fix ISO-8601:1988 week numbers

--- modules/anqh/views/generic/calendar.php
+++ modules/anqh/views/generic/calendar.php
@@ -7,8 +7,6 @@ array_unshift($days, __('wk'));
 $prev = mktime(0, 0, 0, $month - 1, 1, $year);
 $next = mktime(0, 0, 0, $month + 1, 1, $year);
 $curr = mktime(0, 0, 0, $month, 1, $year);
-$week_number = date('W', $curr);
-$week_date = new DateTime();
 
 // Previous and next month query URIs
 $prev = '/events/' . date('Y/m', $prev);
@@ -21,55 +19,59 @@ $next_padding = false;
 <thead>
 	<tr>
 		<td class="prev"><?= html::anchor($prev, '&laquo;') ?></td>
-		<td class="month" colspan="6"><?= html::anchor('/calendar/' . date('Y/m', $curr), strftime('%B %Y', $curr)) ?></td>
+		<td class="month" colspan="6"><?= html::anchor('events/' . date('Y/m', $curr), strftime('%B %Y', $curr)) ?></td>
 		<td class="next"><?= html::anchor($next, '&raquo;') ?></td>
 	</tr>
 	<tr>
-<?php foreach ($days as $day): ?>
-	<th class="week"><?= $day ?></th>
-<?php endforeach ?>
+		<?php foreach ($days as $day): ?>
+		<th class="week"><?= $day ?></th>
+		<?php endforeach; ?>
 	</tr>
 </thead>
 <tbody>
-<?php foreach ($weeks as $week):
-if (empty($week)) continue;
+	<?php foreach ($weeks as $week): if (empty($week)) continue; $print_week = true; ?>
 
-// set week number
-$previous_week = $week_number;
-$week_date->setISODate($year, $week_number++, 4);
-
-// check if week number looped to next year
-//if ($week_date->format('W') < $previous_week) $week_date->setISODate($year + 1, 1);
-//++$week_number;
-?>
 	<tr>
-		<th><?= html::anchor('/events/' . $week_date->format('Y') . '/week/' . $week_date->format('W'), $week_date->format('W')) ?></th>
-<?php 	foreach ($week as $day):
+		<?php foreach ($week as $day):
 
-list($number, $current, $data) = $day;
-if (is_array($data)) {
-	$classes = $data['classes'];
-	$output = empty($data['output']) ? '' : '<ul class="output"><li>' . implode('</li><li>', $data['output']) . '</li></ul>';
-} else {
-	$classes = array();
-	$output = '';
-}
-if (!$current) $classes[] = 'inactive';
-$classes[] = 'day';
+		list($number, $current, $data) = $day;
 
-// check if we are padding days
-$previous_padding = $previous_padding && !$current;
-$next_padding = !$previous_padding && !$current;
+		// Check if we are padding days
+		$previous_padding = $previous_padding && !$current;
+		$next_padding = !$previous_padding && !$current;
 
-// create current day
-$date = mktime(0, 0, 0, $month - (int)$previous_padding + (int)$next_padding, $number, $year);
+		// Create current day
+		$date = mktime(0, 0, 0, $month - (int)$previous_padding + (int)$next_padding, $number, $year);
 
-if (date('Y-n-j') == date('Y-n-j', $date)) $classes[] = 'today';
-$link = '/events/' . date('Y/m/d', $date);
+		// Print week number?
+		if ($print_week) {
+			$week_number = strftime(Kohana::config('locale.start_monday') ? '%V' : '%U', $date);
+			$print_week = false;
+?>
+		<th><?= html::anchor('events/' . date('Y', $date) . '/week/' . $week_number, $week_number) ?></th>
+<?php
+		}
+
+		if (is_array($data)) {
+			$classes = $data['classes'];
+			$output = empty($data['output']) ? '' : '<ul class="output"><li>' . implode('</li><li>', $data['output']) . '</li></ul>';
+		} else {
+			$classes = array();
+			$output = '';
+		}
+		if (!$current) $classes[] = 'inactive';
+		$classes[] = 'day';
+
+
+		if (date('Y-n-j') == date('Y-n-j', $date)) $classes[] = 'today';
+		$link = '/events/' . date('Y/m/d', $date);
+
+		$week_number++;
 ?>
 		<td class="<?= implode(' ', $classes) ?>"><?= html::anchor($link, $number) ?><?= $output ?></td>
-<?php 	endforeach ?>
+		<?php endforeach; ?>
+
 	</tr>
-<?php endforeach ?>
+	<?php endforeach; ?>
 </tbody>
 </table>
