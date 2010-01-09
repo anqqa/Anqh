38a32ad70ad17dfeb76faded897d2c195e217553
anqqa <anttiq@gmail.com>
2010-01-09 19:33:46 +0200

    Tabs as section

--- modules/anqh/views/generic/tabs.php
+++ modules/anqh/views/generic/tabs.php
@@ -1,12 +1,12 @@
 
-<div class="mod tabs" id="<?= $id ?>">
+<section class="mod tabs" id="<?= $id ?>">
 	<ul>
 		<?php	$t = 0; $selected = 0; foreach ($tabs as $tab): $selected = !empty($tab['selected']) ? $t : $selected; $t++; ?>
 		<li><?= html::anchor($tab['href'], $tab['title']) ?></li>
 		<?php	endforeach; ?>
 	</ul>
 	<?php	foreach ($tabs as $tab) echo $tab['tab']; ?>
-</div>
+</section>
 <?php
 // Initialize tabs immediately to aviod ugly jumping
 echo html::script_source('$("#' . $id . '").tabs({ selected: ' . $selected . ', collapsible: true, fx: { height: "toggle", opacity: "toggle", duration: "fast" } });');
