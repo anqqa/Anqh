8996798287d4eb538b1b57eaf7cf28be913947be
anqqa <anttiq@gmail.com>
2010-01-10 22:15:43 +0200

    Peepbox view

---
+++ modules/anqh/views/member/peepbox.php
@@ -0,0 +1,15 @@
+
+<section class="mod peepbox">
+	<header>
+		<h4><?= text::title($member->username) ?></h4>
+	</header>
+
+	<?php if (valid::url($member->picture)): ?>
+	<?= html::image($member->picture, array('width' => 160)); ?>
+	<?php endif; ?>
+
+	<?php if ($member->default_image_id): ?>
+	<?= html::img($member->default_image, 'normal', array('width' => 160)) ?>
+	<?php endif; ?>
+
+</section>
