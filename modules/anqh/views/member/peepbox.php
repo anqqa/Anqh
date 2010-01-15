8cbd905457a4494fd3558e67b74a87e5ea52d60e
anqqa <anttiq@gmail.com>
2010-01-15 21:36:40 +0200

    Layout modularization

--- modules/anqh/views/member/peepbox.php
+++ modules/anqh/views/member/peepbox.php
@@ -4,12 +4,14 @@
 		<h4><?= text::title($member->username) ?></h4>
 	</header>
 
-	<?php if (valid::url($member->picture)): ?>
-	<?= html::image($member->picture, array('width' => 160)); ?>
-	<?php endif; ?>
+	<article>
+		<?php if (valid::url($member->picture)): ?>
+		<?= html::image($member->picture, array('width' => 160)); ?>
+		<?php endif; ?>
 
-	<?php if ($member->default_image_id): ?>
-	<?= html::img($member->default_image, 'normal', array('width' => 160)) ?>
-	<?php endif; ?>
+		<?php if ($member->default_image_id): ?>
+		<?= html::img($member->default_image, 'normal', array('width' => 160)) ?>
+		<?php endif; ?>
+	</article>
 
 </section>
