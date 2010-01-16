37af78728f26556b13747ab0617e13eba224a500
anqqa <anttiq@gmail.com>
2010-01-16 18:37:06 +0200

    K2.4 handles key prefixing

--- modules/anqh/libraries/MY_Cache.php
+++ modules/anqh/libraries/MY_Cache.php
@@ -15,59 +15,54 @@ class Cache extends Cache_Core {
 
 
 	/**
-	 * Delete a cache item by id.
+	 * Delete a cache item by key.
 	 *
-	 * @param   string   $id
+	 * @param   string   $key
 	 * @return  boolean
 	 */
-	public function delete($id) {
-		Cache::$queries['deletes'][] = $id;
-		return parent::delete($id);
+	public function delete($key) {
+		Cache::$queries['deletes'][] = $key;
+		return parent::delete($key);
 	}
 
 
 	/**
-	 * Fetches a cache by id. NULL is returned when a cache item is not found.
+	 * Fetches a cache by key. NULL is returned when a cache item is not found.
 	 *
-	 * @param   string  $id
+	 * @param   string  $key
 	 * @return  mixed   cached data or NULL
 	 */
-	public function get($id) {
-		Cache::$queries['gets'][] = $id;
-		return parent::get($id);
+	public function get($key) {
+		Cache::$queries['gets'][] = $key;
+		return parent::get($key);
 	}
 
 
 	/**
-	 * Return prefixed cache id.
+	 * Return cache key.
 	 *
-	 * @param   string  $id
 	 * @return  string
 	 */
 	public function key() {
-		static $prefix;
-
-		empty($prefix) and $prefix = $this->config['prefix'] . '_';
-
 		$parts = func_get_args();
 
-		return $prefix . implode('_', $parts);
+		return is_array($parts) ? implode('_', $parts) : $parts;
 	}
 
 
 	/**
-	 * Set a cache item by id. Tags may also be added and a custom lifetime
+	 * Set a cache item by key. Tags may also be added and a custom lifetime
 	 * can be set. Non-string data is automatically serialized.
 	 *
-	 * @param   string   $id
+	 * @param   string   $key
 	 * @param   mixed    $data
 	 * @param   array    $tags      use NULL
 	 * @param   integer  $lifetime  number of seconds until the cache expires
 	 * @return  boolean
 	 */
-	public function set($id, $data, $tags = NULL, $lifetime = NULL) {
-		Cache::$queries['sets'][] = $id;
-		return parent::set($id, $data, $tags, $lifetime);
+	public function set($key, $data, $tags = NULL, $lifetime = NULL) {
+		Cache::$queries['sets'][] = $key;
+		return parent::set($key, $data, $tags, $lifetime);
 	}
 
 }
