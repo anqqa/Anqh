015d998826aea4faffaf90b67cf47f9e18446c10
anqqa <anttiq@gmail.com>
2010-01-23 13:44:43 +0200

    Area type update

--- modules/forum/models/forum_area.php
+++ modules/forum/models/forum_area.php
@@ -4,64 +4,53 @@
  *
  * @package    Forum
  * @author     Antti Qvickström
- * @copyright  (c) 2009 Antti Qvickström
+ * @copyright  (c) 2009-2010 Antti Qvickström
  * @license    http://www.opensource.org/licenses/mit-license.php MIT license
  */
 class Forum_Area_Model extends Modeler_ORM {
 
 	/**
+	 * Access to edit area
+	 */
+	const ACCESS_EDIT = 'edit';
+
+	/**
 	 * Area read access
-	 *
-	 * @var  string
 	 */
 	const ACCESS_READ = 'read';
 
 	/**
 	 * Area write access (= new topic)
-	 *
-	 * @var  string
 	 */
 	const ACCESS_WRITE = 'write';
 
 	/**
 	 * Normal area
-	 *
-	 * @var  int
 	 */
 	const TYPE_NORMAL = 0;
 
 	/**
 	 * Read-only area
-	 *
-	 * @var  int
 	 */
 	const TYPE_READONLY = 1;
 
 	/**
 	 * Log-in required area
-	 *
-	 * @var  int
 	 */
 	const TYPE_LOGGED = 2;
 
 	/**
 	 * Private area
-	 *
-	 * @var  int
 	 */
 	const TYPE_PRIVATE = 4;
 
 	/**
-	 * Special area
-	 *
-	 * @var  int
+	 * Bound area, topics bound to other model
 	 */
-	const TYPE_SPECIAL = 8;
+	const TYPE_BIND = 8;
 
 	/**
 	 * Hidden area
-	 *
-	 * @var int
 	 */
 	const TYPE_HIDDEN = 128;
 
@@ -82,17 +71,21 @@ class Forum_Area_Model extends Modeler_ORM {
 
 	protected $url_base = 'forum';
 
+
 	/**
-	 * Check access rights
+	 * Get list of area types
 	 *
-	 * @param   mixed  $user
-	 * @param   int    $type
-	 * @return  bool
-	 *
-	 * @deprecated  in favor of has_access
+	 * @return  array
 	 */
-	public function access_has($user, $type = self::ACCESS_READ) {
-		return $this->has_access($type, $user);
+	public static function area_types() {
+		return array(
+			self::TYPE_NORMAL   => __('Normal area, everybody can read and start new topics'),
+			self::TYPE_READONLY => __('Read only, only moderators can start new topics'),
+			self::TYPE_LOGGED   => __('Members only, only logged in members can read topics'),
+			self::TYPE_PRIVATE  => __('Private area, uses different db (deprecated?)'),
+			self::TYPE_BIND     => __('Bound area, topics are bound to other models'),
+			self::TYPE_HIDDEN   => __('Hidden area, nobody can see'),
+		);
 	}
 
 
@@ -112,27 +105,22 @@ class Forum_Area_Model extends Modeler_ORM {
 			$access = false;
 			switch ($type) {
 
+				// Edit access to area
+				case self::ACCESS_EDIT:
+					$access = ($user && $user->has_role('admin'));
+					break;
+
 				// Read access to area
 				case self::ACCESS_READ:
-					$mask = self::TYPE_HIDDEN;
-
-					// Non-logged has more restrictions
-					if (!$user) $mask |= self::TYPE_LOGGED | self::TYPE_PRIVATE;
-
-					// Mask fits, no access
-					if (!($this->access & $mask)) $access = true;
+					if (!$this->is_type(self::TYPE_HIDDEN)) {
+						$access = ($user || !$this->is_type(self::TYPE_LOGGED | self::TYPE_PRIVATE));
+					}
 					break;
 
 				// Write access to area (= new topic)
 				case self::ACCESS_WRITE:
-
-					// Non-logged can't never write
-					if ($user) {
-						$mask = self::TYPE_READONLY;
-
-						// Mask fits, no access
-						if (!($this->access & $mask)) $access = true;
-
+					if (!$this->is_type(self::TYPE_HIDDEN && $user)) {
+						$access = (!$this->is_type(self::TYPE_READONLY | self::TYPE_BIND) || $user->has_role('admin', 'forum moderator'));
 					}
 					break;
 
@@ -141,6 +129,21 @@ class Forum_Area_Model extends Modeler_ORM {
 		}
 
 		return $cache[$cache_id];
+	}
+
+
+	/**
+	 * Is the area of area type
+	 *
+	 * @param  integer  $access_type
+	 * @see    TYPE_NORMAL
+	 * @see    TYPE_READONLY
+	 * @see    TYPE_LOGGED
+	 * @see    TYPE_PRIVATE
+	 * @see    TYPE_BIND
+	 */
+	public function is_type($access_type) {
+		return $access_type == self::TYPE_NORMAL ? ($this->access === 0) : (bool)($this->access & $access_type);
 	}
 
 
