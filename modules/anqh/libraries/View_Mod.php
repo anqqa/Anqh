01702f4f32538b372beeea01cf1c754e6ec536b2
anqqa <anttiq@gmail.com>
2010-03-14 14:52:27 +0200

    Mod view wrapper

---
+++ modules/anqh/libraries/View_Mod.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Module view
+ *
+ * @package    Anqh
+ * @author     Antti Qvickström
+ * @copyright  (c) 2010 Antti Qvickström
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT license
+ */
+class View_Mod_Core extends View_Core {
+
+	/**
+	 * Creates a new View Mod using the given parameters.
+	 *
+	 * @param   string  view name
+	 * @param   array   pre-load data
+	 * @param   string  type of file: html, css, js, etc.
+	 * @return  object
+	 */
+	public static function factory($name = NULL, $data = NULL, $type = NULL) {
+		return new View_Mod($name, $data, $type);
+	}
+
+
+	/**
+	 * Wrap view with module specific markup
+	 *
+	 * @param   string  $output
+	 * @return  string
+	 */
+	public function wrap($output) {
+		$data = array(
+			'id'      => arr::get($this->kohana_local_data, 'mod_id'),
+			'class'   => trim('mod ' . arr::get($this->kohana_local_data, 'mod_class')),
+			'content' => $output,
+		);
+
+		return (string)View::factory('generic/mod', $data);
+	}
+
+
+	/**
+	 * Magically converts mod view object to string.
+	 *
+	 * @return  string
+	 */
+	public function __toString() {
+		try {
+			return $this->render(false, array($this, 'wrap'));
+		} catch (Exception $e) {
+			Kohana_Exception::handle($e);
+			return '';
+		}
+	}
+
+}
---
+++ modules/anqh/views/generic/mod.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * View Mod
+ *
+ * @package    Anqh
+ * @author     Antti Qvickström
+ * @copyright  (c) 2010 Antti Qvickström
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT license
+ */
+?>
+
+<section class="<?= $class ?>"<?= $id ? ' id="' . $id . '"' : '' ?>>
+	<div>
+
+<?= $content ?>
+
+	</div>
+</section><!-- <?= $class ?> -->
